9462ee634c4f1952456da6b5c5b2316456cb4c18
Starrah <46821571+Starrah@users.noreply.github.com>
2020-04-24 16:27:43 +0800

    Initial Commit

---
+++ app/src/main/java/cn/starrah/thu_course_helper/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.starrah.thu_course_helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
