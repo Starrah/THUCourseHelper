d88ae4dcdd663fc658d28e423593421b3afc0606
Starrah <46821571+Starrah@users.noreply.github.com>
2020-05-31 22:53:59 +0800

    优化Preference的显示，加入提示和登录按钮显示处理 在build.gradle中显式声明目标jvm版本，解决kotlin编译版本带来的问题

--- app/src/main/java/cn/starrah/thu_course_helper/fragment/SettingsFragment.kt
+++ app/src/main/java/cn/starrah/thu_course_helper/fragment/SettingsFragment.kt
@@ -2,27 +2,44 @@ package cn.starrah.thu_course_helper.fragment
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.preference.*
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import cn.starrah.thu_course_helper.R
 
-class SettingsFragment: PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat() {
     lateinit var sp: SharedPreferences
-    lateinit var pre_sync_learnx: SwitchPreferenceCompat
+    lateinit var pf_sync_learnx: SwitchPreferenceCompat
+    lateinit var pf_login: Preference
     val spListener = SharedPreferences.OnSharedPreferenceChangeListener { sp, key ->
-        if (key == "login_status") pre_sync_learnx.isEnabled = sp.getInt(key, 0) == 2
+        if (key == "login_status") {
+            val login_status = sp.getInt(key, 0)
+            pf_login.title =
+                if (login_status == 0) resources.getString(R.string.settings_not_login)
+                else sp.getString("login_name", "")
+            pf_login.summary = when (login_status) {
+                1 -> "已登录"
+                2 -> "已登录，已保存密码"
+                else -> ""
+            }
+            pf_sync_learnx.isEnabled = login_status == 2
+            if (!pf_sync_learnx.isEnabled) pf_sync_learnx.summary = resources.getString(R.string.errmsg_open_sync_learnx)
+
+        }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         sp = preferenceManager.sharedPreferences
 
-        val pre_login = findPreference<Preference>("login_status")!!
-        pre_login.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+        pf_login = findPreference<Preference>("login_status")!!
+        pf_login.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             LoginDialog(requireActivity()).show()
             true
         }
 
-        pre_sync_learnx = findPreference("sync_learnx_homeworks")!!
+        pf_sync_learnx = findPreference("sync_learnx_homeworks")!!
+
         spListener.onSharedPreferenceChanged(sp, "login_status")
 
     }
