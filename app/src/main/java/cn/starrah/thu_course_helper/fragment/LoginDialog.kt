b90d09021058b8c87316f341fb6b814780a863ca
Charles Shen <ssa19990208@foxmail.com>
2020-06-17 15:35:26 +0800

    增加了显示/隐藏密码和进度条

--- app/src/main/java/cn/starrah/thu_course_helper/fragment/LoginDialog.kt
+++ app/src/main/java/cn/starrah/thu_course_helper/fragment/LoginDialog.kt
@@ -3,12 +3,15 @@ package cn.starrah.thu_course_helper.fragment
 import android.app.Dialog
 import android.content.Context
 import android.text.Editable
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import androidx.core.content.edit
+import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
@@ -16,8 +19,12 @@ import cn.starrah.thu_course_helper.R
 import cn.starrah.thu_course_helper.onlinedata.thu.THUCourseDataSouce
 import kotlinx.coroutines.launch
 
-class LoginDialog(context: Context) : Dialog(context) {
+class LoginDialog(context: Context) : Dialog(context){
     private var theContext: Context? = null
+    private lateinit var idPlace: EditText
+    private lateinit var passPlace:EditText
+    private lateinit var loginBar:ProgressBar
+    private lateinit var loginBarPlace:LinearLayout
     private fun initDialog(context: Context) {
         theContext = context
         buildDialog()
@@ -25,6 +32,7 @@ class LoginDialog(context: Context) : Dialog(context) {
 
     private fun buildDialog() {
         val layout = LayoutInflater.from(theContext!!).inflate(R.layout.login, null)
+
         PreferenceManager.getDefaultSharedPreferences(theContext).let {
             if (it.getInt("login_status", 0) > 0) {
                 it.getString("login_name", null)?.let {
@@ -32,46 +40,107 @@ class LoginDialog(context: Context) : Dialog(context) {
                 }
             }
         }
-        layout.findViewById<Button>(R.id.login_ok).setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View?) {
-                (theContext as FragmentActivity).lifecycleScope.launch {
-                    var login_id_place: EditText = layout.findViewById(R.id.login_id)
-                    var login_pass_place: EditText = layout.findViewById(R.id.login_password)
 
-                    var login_id: String = login_id_place.text.toString()
-                    var login_pass: String = login_pass_place.text.toString()
+        idPlace = layout.findViewById(R.id.login_id)
+        passPlace = layout.findViewById(R.id.login_password)
+        loginBar = layout.findViewById(R.id.login_bar)
+        loginBarPlace = layout.findViewById(R.id.login_bar_place)
 
-                    if (login_id.isEmpty()) {
-                        Toast.makeText(theContext!!, "账号不能为空！", Toast.LENGTH_SHORT).show()
-                    } else if (login_pass.isEmpty()) {
-                        Toast.makeText(theContext!!, "密码不能为空！", Toast.LENGTH_SHORT).show()
-                    } else {
-                        try {
-                            THUCourseDataSouce.login(
-                                theContext as FragmentActivity, login_id,
-                                login_pass
-                            )
-                            PreferenceManager.getDefaultSharedPreferences(theContext).edit {
-                                putInt("login_status", 1)
-                                putString("login_name", login_id)
-                                putInt("login_force_update", (System.currentTimeMillis() / 1000).toInt())
+        //隐藏progressbar
+        var params_hide: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0)
+        loginBarPlace.setLayoutParams(params_hide);
+        loginBar.isVisible = false
+
+        layout.findViewById<Button>(R.id.login_ok)
+            .setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View?) {
+                    (theContext as FragmentActivity).lifecycleScope.launch {
+
+
+                        var login_id: String = idPlace.text.toString()
+                        var login_pass: String = passPlace.text.toString()
+
+                        if (login_id.isEmpty()) {
+                            Toast.makeText(theContext!!, "账号不能为空！", Toast.LENGTH_SHORT).show()
+                        }
+                        else if (login_pass.isEmpty()) {
+                            Toast.makeText(theContext!!, "密码不能为空！", Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            try {
+                                //显示progressbar
+                                var params_show: LinearLayout.LayoutParams =
+                                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                                loginBarPlace.setLayoutParams(params_show);
+                                loginBar.isVisible = true
+
+                                THUCourseDataSouce.login(
+                                    theContext as FragmentActivity, login_id,
+                                    login_pass
+                                )
+                                PreferenceManager.getDefaultSharedPreferences(theContext).edit {
+                                    putInt("login_status", 1)
+                                    putString("login_name", login_id)
+                                    putInt(
+                                        "login_force_update",
+                                        (System.currentTimeMillis() / 1000).toInt()
+                                    )
+                                }
+
+                                //隐藏progressbar
+                                var params_hide: LinearLayout.LayoutParams =
+                                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0)
+                                loginBarPlace.setLayoutParams(params_hide);
+                                loginBar.isVisible = false
+
+                                Toast.makeText(
+                                    theContext,
+                                    R.string.login_success,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                dismiss()
                             }
-                            Toast.makeText(theContext, R.string.login_success, Toast.LENGTH_SHORT).show()
-                            dismiss()
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            //TODO 更详细的异常处理
-                            Toast.makeText(theContext!!, "账号或密码错误，登陆失败！", Toast.LENGTH_SHORT).show()
+                            catch (e: Exception) {
+                                e.printStackTrace()
+
+                                //隐藏progressbar
+                                var params_hide: LinearLayout.LayoutParams =
+                                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0)
+                                loginBarPlace.setLayoutParams(params_hide);
+                                loginBar.isVisible = false
+
+                                Toast.makeText(theContext!!, "登陆失败！", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
                         }
                     }
                 }
+            })
+        layout.findViewById<Button>(R.id.login_no)
+            .setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View?) {
+                    dismiss()
+                }
+            })
+
+        //显示/隐藏密码
+        layout.findViewById<ImageButton>(R.id.show_hide_button).setOnTouchListener(View.OnTouchListener { view, motionEvent ->
+            when (motionEvent.action){
+                MotionEvent.ACTION_DOWN -> {
+                    passPlace.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    view.background = context.getDrawable(R.drawable.icon_show_pass)
+                }
+                MotionEvent.ACTION_UP -> {
+                    passPlace.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    view.background = context.getDrawable(R.drawable.icon_hide_pass)
+                }
             }
+            return@OnTouchListener true
         })
-        layout.findViewById<Button>(R.id.login_no).setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View?) {
-                dismiss()
-            }
-        })
+
+
+
         setContentView(layout)
     }
 
@@ -79,3 +148,4 @@ class LoginDialog(context: Context) : Dialog(context) {
         initDialog(context)
     }
 }
+
