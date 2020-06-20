25ad7d79d8ad4e173234c87f0c0bdbdfd29adb5a
Charles Shen <ssa19990208@foxmail.com>
2020-06-20 14:01:53 +0800

    fix

---
+++ app/src/main/java/cn/starrah/thu_course_helper/information/ExamShowActivity.kt
@@ -0,0 +1,76 @@
+package cn.starrah.thu_course_helper.information
+
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import cn.starrah.thu_course_helper.R
+import cn.starrah.thu_course_helper.TableFragment
+import cn.starrah.thu_course_helper.activity.ItemEditActivity
+import cn.starrah.thu_course_helper.data.database.CREP
+import cn.starrah.thu_course_helper.data.declares.calendarEntity.CalendarItemDataWithTimes
+import cn.starrah.thu_course_helper.data.declares.calendarEntity.CalendarTimeData
+import cn.starrah.thu_course_helper.data.declares.calendarEntity.CalendarTimeDataWithItem
+import cn.starrah.thu_course_helper.data.declares.calendarEnum.CalendarItemLegalDetailKey
+import cn.starrah.thu_course_helper.data.declares.calendarEnum.CalendarItemType
+import cn.starrah.thu_course_helper.data.declares.calendarEnum.CalendarRemindType
+import cn.starrah.thu_course_helper.data.declares.calendarEnum.CalendarTimeType
+import cn.starrah.thu_course_helper.data.utils.chineseName
+import cn.starrah.thu_course_helper.data.utils.getNotNullValue
+import kotlinx.coroutines.launch
+import java.time.format.DateTimeFormatter
+
+class ExamShowActivity : AppCompatActivity(){
+
+    private var showItem: ArrayList<CalendarItemDataWithTimes> = arrayListOf()
+
+
+    /**
+     * 描述：根据id从数据库获取数据，id是类变量showID，读取的数据存在showItem里
+     * 参数：无
+     * 返回：无
+     */
+    suspend fun getData() {
+        //数据获取
+        var the_item = CREP.helper_findHomeworkItems()
+        showItem.addAll(the_item)
+    }
+
+    /**
+     * 描述：初始化
+     * @param savedInstanceState 存储的data，其实只有待显示活动的ID
+     */
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calendar_item_show)
+
+
+        val intent = intent
+
+        lifecycleScope.launch {
+            getData()
+            //TODO 排序
+            //TODO 显示
+        }
+    }
+
+    /**
+     * 描述：处理返回按钮的事件--返回
+     * 参数：无
+     * 返回：无
+     */
+    fun handleReturn(view: View) {
+        finish()
+    }
+
+
+}
--- app/src/main/java/cn/starrah/thu_course_helper/information/HomeworkShowActivity.java
+++
@@ -1,6 +0,0 @@
-package cn.starrah.thu_course_helper.information;
-
-import android.app.Activity;
-
-public class HomeworkShowActivity extends Activity {
-}
